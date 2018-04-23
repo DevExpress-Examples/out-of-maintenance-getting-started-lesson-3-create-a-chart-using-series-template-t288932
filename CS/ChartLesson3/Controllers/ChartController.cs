ef248aba9de260db28c5e774104e950dd1578b90
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ChartLesson3/Controllers/ChartController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChartLesson3.Controllers {
+    public class ChartController : Controller {
+        public ActionResult Index() {
+            using (var dbContext = new Models.GspDbContext()) {
+                return View(dbContext.GSPs.ToList());
+            }
+        }
+    }
+}
---
+++ VB/ChartLesson3/Controllers/ChartController.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+
+Namespace ChartLesson3.Controllers
+    Public Class ChartController
+        Inherits Controller
+
+        Public Function Index() As ActionResult
+            Using dbContext = New Models.GspDbContext()
+                Return View(dbContext.GSPs.ToList())
+            End Using
+        End Function
+    End Class
+End Namespace
